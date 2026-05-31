d51b4384ba02c8a58e7b23f5cd47945501506d68
Patrick Bieser <patrickbieser@gmail.com>
2026-05-31 11:38:01 -0500

    Bugle May 31, 2026

--- Go.ps1
+++ Go.ps1
@@ -3,6 +3,6 @@
   -MarkdownPath .\bugle.md `
   -CalendarHtmlPath .\calendar.html `
   -OutPath .\output\bugle.html `
-  -BugleDate "May 15, 2026" `
-  -HeroImage "https://Troop79.b-cdn.net/CompassCourse.png" `
+  -BugleDate "May 31, 2026" `
+  -HeroImage "https://Troop79.b-cdn.net/Green-Bush-GroupOnLog.png" `
   -LogoImage "https://Troop79.b-cdn.net/Bugle%20Logo.png"
