6ec11f5a01779b383322e43d78756f3ed0e747d0
Patrick Bieser <patrickbieser@gmail.com>
2026-07-19 12:33:32 -0500

    Bugle July 19. 2026

--- Go.ps1
+++ Go.ps1
@@ -3,6 +3,6 @@
   -MarkdownPath .\bugle.md `
   -CalendarHtmlPath .\calendar.html `
   -OutPath .\output\bugle.html `
-  -BugleDate "July 12, 2026" `
+  -BugleDate "July 19, 2026" `
   -HeroImage "https://Troop79.b-cdn.net/TesomasScoutCampSign.png" `
   -LogoImage "https://Troop79.b-cdn.net/Bugle%20Logo.png"
