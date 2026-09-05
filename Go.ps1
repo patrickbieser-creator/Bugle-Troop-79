a297108b5e94a4da51352b7121a1196d0019ab1d
Patrick Bieser <patrickbieser@gmail.com>
2026-09-05 18:35:02 -0500

    Bugle Sept 5, 2026

--- Go.ps1
+++ Go.ps1
@@ -2,6 +2,7 @@
   -TemplatePath .\bugle-template.html `
   -MarkdownPath .\bugle.md `
   -OutPath .\output\bugle.html `
-  -BugleDate "August 30, 2026 " `
+  -BugleDate "September 5, 2026 " `
   -HeroImage "https://Troop79.b-cdn.net/courtofhonr2025-16bc7bde-1200-785839b6.jpg" `
   -LogoImage "https://Troop79.b-cdn.net/Bugle%20Logo.png"
+ 
